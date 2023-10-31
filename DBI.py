35c0b1761bbe603ba7aa5feca004a42ac0b0c177
John (Shane) Lewin <john.lewin@gmail.com>
2023-10-30 21:16:39 -0700

    more improvements to DBI

--- DBI.py
+++ DBI.py
@@ -23,29 +23,33 @@ class DataBackendInterface:
         return con
         
     def check_if_email_exists(self, email):
-        ''' check that this email is not already in the db '''
+        ''' check if a user email is already in the db '''
 
-        
         con = self.create_connection()
         cur = con.cursor()
+
+        # check if there's a user in the DB
         sqlstr = "select count(*) from users where email='{email}'".format(
             email=email.lower())
         cur.execute(sqlstr)
         cnt = cur.fetchone()[0]
+        con.close()
+
         if cnt > 0:
             return True
         return False 
-        
 
-    def addUser(self, email, role=None, metadata=None):
+    def add_user(self, email, role=None, metadata=None):
         '''
         Add a new user to the DB. Default role is user.
         '''
-        con = self.get_connection_object()
-        cur = con.cursor()
 
+        # check that this user is unique
+        if self.check_if_email_exists(email.lower()): 
+            raise Exception("%s is already in the database"%email)
         
-        
+        con = self.create_connection()
+        cur = con.cursor()
 
         # assign defaults 
         user_id = str(uuid4())
@@ -53,6 +57,7 @@ class DataBackendInterface:
             role = "user"
         status= "active"
 
+        # format sql
         sqlstr = '''insert into Users(user_id, email, role, status, metadata)
         values("{user_id}", 
                 "{email}", 
@@ -67,10 +72,39 @@ class DataBackendInterface:
                     metadata=metadata
                 )
         
-        
+        # load
+        cur.execute(sqlstr)
+        r = con.commit()
+        con.close()
+        return r
+    
+    def add_user_group(self, name):
+        raise Exception(NotImplemented)
+    
+    def assign_user_to_group(self, email, group_name):
+        raise Exception(NotImplemented)
+    
+    def remove_user_from_group(self, email, group_name):
+        raise Exception(NotImplemented)
+    
+    def create_program(self, name, data, appliation, user_group, overlap, priority=None):
+        raise Exception(NotImplemented)
+    
+    def start_program(self, program_name):
+        raise Exception(NotImplemented)
+    
+    def pause_program(self, program_name):
+        raise Exception(NotImplemented)
+    
+    
+
 if __name__ == '__main__': 
 
     dbi = DataBackendInterface()
     x = dbi.check_if_email_exists("john.s.lewin@gmail.com")
+    print(x)
+
+    dbi.addUser("john.s.lewin@gmail.com")
+    x = dbi.check_if_email_exists("john.s.lewin@gsk.com")
     print(x)
     bp = True
